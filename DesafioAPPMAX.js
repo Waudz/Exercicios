041fdce3de2dde15f58b60bc32ce78dbf1c50a20
William de Almeida <98135559+Waudz@users.noreply.github.com>
2022-01-23 10:11:40 -0300

    Update DesafioAPPMAX.js

--- DesafioAPPMAX.js
+++ DesafioAPPMAX.js
@@ -22,7 +22,6 @@ class cozinha {
         this.armazemarIngredientes.push()
     }
     anunciarArmazenarIngredientes() {
-        //console.log(this.armazemarIngredientes)
         console.log(`Quantidade de ingredientes: ${this.armazemarIngredientes.length}`)
     }
 
@@ -52,27 +51,29 @@ class ingrediente {
     
     console.log("\nLista de ingredientes:")
 
-    var data1a = new Date(2022, 04, 30)
+    var data1a = new Date(2022, 03, 30).toLocaleDateString('pt-BR')
+    // Não consegui fazer com que a data ficasse no formato brasileiro quando testado o replit.com, (no Visual Studio Code ela ficou no formato brasileiro).
+    // Tentei contornar isso de várias maneiras porém sem sucesso, então, por fim, decidi manter no formato americano.
     const ingrediente1a = new ingrediente('Feijão', data1a)
     ingrediente1a.anunciarNomeIngrediente()
     ingrediente1a.anunciarDataValidade()
     
-    var data2a = new Date(2022, 05, 04)
+    var data2a = new Date(2022, 04, 04).toLocaleDateString('pt-BR')
     const ingrediente2a = new ingrediente('Farinha', data2a)
     ingrediente2a.anunciarNomeIngrediente()
     ingrediente2a.anunciarDataValidade()
     
-    var data3a = new Date(2022, 04, 27)
+    var data3a = new Date(2022, 03, 27).toLocaleDateString('pt-BR')
     const ingrediente3a = new ingrediente('Arroz', data3a)
     ingrediente3a.anunciarNomeIngrediente()
     ingrediente3a.anunciarDataValidade()
     
-    var data4a = new Date(2022, 03, 15)
+    var data4a = new Date(2022, 02, 15).toLocaleDateString('pt-BR')
     const ingrediente4a = new ingrediente('Carne de Porco', data4a)
     ingrediente4a.anunciarNomeIngrediente()
     ingrediente4a.anunciarDataValidade()
     
-    var data5a = new Date(2022, 04, 03)
+    var data5a = new Date(2022, 03, 03).toLocaleDateString('pt-BR')
     const ingrediente5a = new ingrediente('Linguiça', data5a)
     ingrediente5a.anunciarNomeIngrediente()
     ingrediente5a.anunciarDataValidade()
@@ -91,23 +92,23 @@ console.log("\n")
     
     console.log("\nLista de ingredientes:")
     
-    var data1b = new Date(2022, 02, 27)
+    var data1b = new Date(2022, 01, 27).toLocaleDateString('pt-BR')
     const ingrediente1b = new ingrediente('Champignon,', data1b)
     ingrediente1b.anunciarNomeIngrediente()
     ingrediente1b.anunciarDataValidade()
     
-    var data2b = new Date(2022, 01, 25)
+    var data2b = new Date(2022, 00, 25).toLocaleDateString('pt-BR')
     const ingrediente2b = new ingrediente('Brócolis,', data2b)
     ingrediente2b.anunciarNomeIngrediente()
     ingrediente2b.anunciarDataValidade()
 
-    var data3b = new Date(2022, 04, 25)
+    var data3b = new Date(2022, 03, 25).toLocaleDateString('pt-BR')
     const ingrediente3b = new ingrediente('Macarrão', data3b)
     ingrediente3b.anunciarNomeIngrediente()
     ingrediente3b.anunciarDataValidade()
 
-    var data4b = new Date(2022, 03, 17)
+    var data4b = new Date(2022, 02, 17).toLocaleDateString('pt-BR')
     const ingrediente4b = new ingrediente('Carne', data4b)
     ingrediente4b.anunciarNomeIngrediente()
     ingrediente4b.anunciarDataValidade()
-} 
+}
